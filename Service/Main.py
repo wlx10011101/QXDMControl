05135c8581e1a726365895c84a5c8bee97141a77
weilingxiang <wei.lingxiang@zte.com.cn>
2017-02-28 14:45:37 +0000

    main function

--- Service/Main.py
+++ Service/Main.py
@@ -4,15 +4,19 @@ Created on 20170228
 
 @author: WLX
 '''
+from __builtin__ import str
 
 if __name__ == '__main__':
-    separator = '-----------------------------'
+    separator = '-----------------------------\r\n'
     
-    note = 'This Application is just for \r\n\
-            sending the command to QXDM \r\n\
-            that can control UE attach \r\n\
-            and detach\r\n'
-
-    function = '*please start Server First!*'
+    note = ['This Application is just for',
+            'sending the command to QXDM',
+            'that can control UE attach', 
+            'and detach',]
+    function = '*please start Server First!*\r\n'
+    print separator,
+    for str in note:
+        print str
+    print separator, function
     
-    print note
+    qxdmType = raw_input("which ")
